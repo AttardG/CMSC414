d06bc262095c7f66e34939e64813cd7f7ebfb5eb
Semiotic <78098764+christianj2k@users.noreply.github.com>
2023-03-17 21:00:14 -0400

    Start of DNS GUI
    Start of GUI task for the DNS Poisoning program

---
+++ src/DNS_GUI/DNS_GUI.py
@@ -0,0 +1,258 @@
+import sys
+import os
+import subprocess
+import re
+import datetime
+import shutil
+from tkinter import filedialog
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+class dns_window(QMainWindow):
+
+    #Constructor
+    def __init__(self):
+        super().__init__()
+        self.htmloption = False
+        self.running = False
+        title = 'Recluse'
+        self.setWindowTitle(title)
+        self.setGeometry(0,0,500,300) #Set window size
+        self.center()
+        self.baraction()
+        self.dnsmenubar()
+        self.connectbar()
+        self.labels()
+        self.buttons()
+        self.show()
+    def center(self):
+        qr = self.frameGeometry() #Get information about the location and size of the window
+        cp = QDesktopWidget().availableGeometry().center() #Determine the center position of the host monitor screen
+        qr.moveCenter(cp) #Move rectangle position to center
+        self.move(qr.topLeft()) #Move window to the rectangle position
+    
+    ##MenuBar
+    def dnsmenubar(self):
+        mbar = QMenuBar(self) #Create menu bar object
+        self.setMenuBar(mbar) #Add menu bar object to window
+        #Create new menu entities, "&Name" so entity is underlined
+        filemenu = QMenu("&File",self)
+        program = QMenu("&Program",self)
+        view = QMenu("&View",self)
+        helps = QMenu("&Help",self)
+        #Add actions to menu entities
+        program.addAction(self.startaction)
+        program.addAction(self.stopaction)
+        program.addAction(self.exitaction)
+        filemenu.addAction(self.uploadaction)
+        view.addAction(self.logaction)
+        view.addAction(self.sqlaction)
+        helps.addAction(self.helpaction)
+        helps.addAction(self.aboutaction)
+        #Add menu entities to menu object
+        mbar.addMenu(program)
+        mbar.addMenu(filemenu)
+        mbar.addMenu(view)
+        mbar.addMenu(helps)
+    def baraction(self):
+        #Create action options under the menu entities
+        self.startaction = QAction(QIcon("start.png"),"&Start",self)
+        self.stopaction = QAction(QIcon("stop.png"), "&Stop",self)
+        self.exitaction = QAction("&Exit",self)
+        self.uploadaction = QAction("&Upload HTML",self)
+        self.logaction = QAction("&View Log",self)
+        self.sqlaction = QAction("&View MySql",self)
+        self.helpaction = QAction("&Help",self)
+        self.aboutaction = QAction("&About",self)
+    def connectbar(self):
+        #Set functions that will be triggered by the action options
+        self.startaction.triggered.connect(self.start)
+        self.stopaction.triggered.connect(self.stop)
+        self.exitaction.triggered.connect(self.close)
+        self.uploadaction.triggered.connect(self.upload)
+        self.logaction.triggered.connect(self.viewlog)
+        self.sqlaction.triggered.connect(self.viewsql)
+        self.helpaction.triggered.connect(self.help)
+        self.aboutaction.triggered.connect(self.about)
+    def start(self):
+        self.Start()
+    def stop(self):
+        self.Stop()
+    def upload(self):
+        filename = filedialog.askopenfilename() #Open file explorer to find a particular file
+        htmldir = os.getcwd() #Returns current directory of program
+        if len(filename) > 0:
+            shutil.copy(filename,f"{htmldir}/HTMLSpoofs") #Copy HTML/php file to HTMLSpoofs directory
+    def viewlog(self):
+        hold
+    def viewsql(self):
+        hold
+    def help(self):
+        helpbox = QMessageBox(self)
+        helpbox.setWindowTitle("Help")
+        helptext = "To run click start, to stop click stop"
+        helpbox.setText(helptext)
+        helpbox.exec()
+    def about(self):
+        aboutbox = QMessageBox(self)
+        aboutbox.setWindowTitle("About")
+        abouttext = "A VCU project"
+        aboutbox.setText(abouttext)
+        aboutbox.exec()
+
+    #HostIp/MAC
+    def netretrieve(self):
+        try: 
+
+            ipconfig = subprocess.run(['ipconfig','/all'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+            ipconfig_lines = ipconfig.splitlines()
+            ipconfig_lines = [line.replace(" ","") for line in ipconfig_lines if 1==1]
+            search_key = ipconfig_lines[8].split(":")
+
+            found = 0
+            x = 0
+            MAC = ""
+            IP = ""
+            Subnet = ""
+            while found == 0:
+                if(ipconfig_lines[x] == f'Connection-specificDNSSuffix.:{search_key[1]}'):
+                    y = x
+                    found = 1
+                    temp = ipconfig_lines[y:y+22]
+                    MAC = temp[2]
+                    IP = temp[8]
+                    Subnet = temp[9]
+                x = x+1
+
+            MACnum = MAC[24:]; MACnum = MACnum.replace(":","")
+            IPnum = IP[22:];  IPnum = IPnum.replace(":",""); IPnum = IPnum.replace("(Preferred)","")
+            Subnum = Subnet[21:]; Subnum = Subnum.replace(":","")
+            if(IP[:11] != "IPv4Address" or Subnet[:10] != "SubnetMask" or len(IPnum) < 4 or len(Subnum) < 4):
+                raise Exception("An error occurred, please check your network")
+
+            arptable = subprocess.run(['arp','-a'], stdout=subprocess.PIPE).stdout.decode('utf-8').splitlines()
+            found = 0
+            x = 0
+            IPs_MAC = []
+            tempARP = []
+            while found == 0:
+                if(re.match(f'Interface: {IPnum}(.)',arptable[x])):
+                    x = x + 2
+                    found = 1
+                    while arptable[x] != '':
+                        temp = arptable[x].split(" ")
+                        for line in temp:
+                            if(line != ''):
+                                if(line == 'dynamic'):
+                                    IPs_MAC.append(tempARP[0]); IPs_MAC.append(tempARP[1])
+                                    tempARP.clear()
+                                elif(line == 'static'):
+                                    tempARP.clear()
+                                else:
+                                    tempARP.append(line)
+                        x = x+1
+                x = x+1
+            IPs_MAC.append(IPnum); IPs_MAC.append(MACnum)
+            return IPs_MAC
+
+        except:
+            print("An error occurred, retry or check your network config")
+
+    def IPsMAC(self):
+        IpsMACs = self.netretrieve()
+        HostIP_MAC = []
+        HostIP_MAC.append(IpsMACs[len(IpsMACs)-2]); HostIP_MAC.append(IpsMACs[len(IpsMACs)-1]); 
+        self.hostLabel.setText(f"Host IP: {HostIP_MAC[0]}\nHost MAC: {HostIP_MAC[1]}")
+        self.hostLabel.setAlignment(Qt.AlignLeft)
+        self.hostLabel.resize(200,30)
+        self.hostLabel.move(10,30)
+        self.lists.clear()
+        for x in range(0, len(IpsMACs)-2, 2):
+            IP = QListWidgetItem(f"IP: {IpsMACs[x]} MAC: {IpsMACs[x+1]}")
+            self.lists.addItem(IP)
+            
+
+    #Labels&Buttons
+    def labels(self):
+        self.hostLabel = QLabel(self) #Create Label object
+        self.lists = QListWidget(self) #Create list object
+        self.lists.setGeometry(30,50,300,135) #Set list size
+        self.lists.move(10,100) #Set List position
+        self.lists2 = QListWidget(self)
+        self.lists2.setGeometry(30,50,300,50)
+        self.lists2.move(10,240)
+        self.domainLabel = QLabel("Domains to Poison",self)
+        self.domainLabel.setAlignment(Qt.AlignCenter)
+        self.domainLabel.move(320,70)
+        self.domainLabel2 = QLabel("HTML to Spoof",self)
+        self.domainLabel2.setAlignment(Qt.AlignCenter)
+        self.domainLabel2.move(320,190)
+        
+    def buttons(self):
+        self.ipbtn = QPushButton("LocateIPs",self) #Create regular button
+        self.ipbtn.clicked.connect(self.IPsMAC)
+        self.ipbtn.move(10,60)
+
+        self.start = QPushButton("Start",self)
+        self.start.clicked.connect(self.Start)
+        self.start.move(110,60)
+        self.stop = QPushButton("Stop",self)
+        self.stop.clicked.connect(self.Stop)
+        self.stop.move(210,60)
+
+        self.dropdown = QComboBox(self) #Dropdown box button
+        self.dropdown.addItem("All Domains")
+        domains = open('domains.txt','r')
+        urls = domains.readlines()
+        for url in urls:
+            self.dropdown.addItem(url.strip())
+        self.dropdown.move(320,100)
+
+        self.spoofHTML = QRadioButton("Spoof HTML",self) #Create bubble select button
+        self.dontSpoof = QRadioButton("Dont spoof HTML",self)
+        self.spoofHTML.setChecked(True)
+        self.spoofHTML.toggled.connect(lambda:self.set_spoof(self.spoofHTML))
+        self.dontSpoof.toggled.connect(lambda:self.set_spoof(self.dontSpoof))
+        self.spoofHTML.move(320,140)
+        self.dontSpoof.move(320,160)
+
+        self.dropdown2 = QComboBox(self)
+        self.dropdown2.addItem("All Domains")
+        domainSpoof = open('domainSpoof.txt','r')
+        urlSpoof = domainSpoof.readlines()
+        for url in urlSpoof:
+            self.dropdown2.addItem(url.strip())
+        self.dropdown2.move(320,220)
+    
+    #Spoof/Poison Functions
+    def set_spoof(self,radio):
+        if radio.text() == "Spoof HTML":
+            if radio.isChecked() == True:
+                htmloption = True
+            else:
+                htmloption = False
+    def Start(self):
+        if self.running == False:
+            self.running = True
+            time = datetime.datetime.now()
+            self.lists2.addItem(f"Poisoning has begun {time}")
+
+    def Stop(self):
+        if self.running == True:
+            self.running = False
+            time = datetime.datetime.now()
+            self.lists2.addItem(f"Poisoning has ended {time}")
+    
+    #Error box
+    def errorbox(self):
+        error = QMessageBox(self)
+        error.setWindowTitle("ERROR")
+        error.setText("Something went wrong")
+        error.exec()
+
+        
+    
+app = QApplication(sys.argv) #Create new application
+window = dns_window() #Create new window object for the gui
+sys.exit(app.exec()) #Run application loop to run the gui
